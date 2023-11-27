e50441698891495b9ac3fb2873644caa995f7f62
Blazeo7 <moravcikamtus343@gmail.com>
2023-11-27 18:05:18 +0100

    Implement operator precedence parsing

--- src/expr_parser.c
+++ src/expr_parser.c
@@ -1,12 +1,320 @@
 /**
  * @file expr_parser.c
  * @author Jakub Kloub, xkloub03, VUT FIT
+ * @author Matúš Moravčík, xmorav48, FIT VUT
+ * @date 27/11/2023
  */
 #include "expr_parser.h"
+#include <string.h>
 #include "error.h"
+#include "pushdown.h"
 
-bool expr_parser_begin(Token first_token) {
-    (void)first_token;
-    MASSERT(false, "TODO");
+const char* RULES[] = {
+    "i", "(E)", "-E", "i!", "E+E", "E*E", "E>E", "E,E", "L,E", "i(L)", "i(E)", "i()", "i:E",
+};
+
+const Rule RULE_NAMES[] = {
+    Rule_Identif, Rule_Paren,  Rule_Prefix,          Rule_Postfix, Rule_SumSub,  Rule_MulDiv,   Rule_Logic,
+    Rule_ArgsEE,  Rule_ArgsLE, Rule_FnArgsProcessed, Rule_FnArgs,  Rule_FnEmpty, Rule_NamedArg,
+};
+
+const ComprarisonResult PRECEDENCE_TABLE[11][11] = {
+    {Left, Right, Left, Right, Right, Right, Left, Right, Left, Left, Left},    /* +- */
+    {Left, Left, Left, Right, Right, Right, Left, Right, Left, Left, Left},     /* * */
+    {Right, Right, Left, Right, Right, Right, Left, Right, Left, Left, Left},   /* logic */
+    {Left, Left, Left, Err, Right, Right, Left, Right, Left, Left, Left},       /* pre */
+    {Left, Left, Left, Left, Err, Err, Left, Right, Left, Left, Left},          /* post */
+    {Right, Right, Right, Right, Right, Right, Equal, Right, Right, Left, Err}, /* ( */
+    {Left, Left, Left, Left, Left, Err, Left, Err, Left, Left, Left},           /* ) */
+    {Left, Left, Left, Left, Equal, Equal, Left, Err, Left, Equal, Left},       /* id */
+    {Right, Right, Right, Right, Right, Right, Left, Right, Left, Err, Err},    /* ,*/
+    {Right, Right, Right, Right, Right, Right, Left, Right, Left, Err, Err},    /* : */
+    {Right, Right, Right, Right, Right, Right, Right, Right, Err, Err, Err}};   /* $ */
+
+bool expr_parser_begin(Token token, Data* data) {
+  Pushdown* pushdown = malloc(sizeof(Pushdown));
+  pushdown_init(pushdown);
+
+  parse(pushdown, token, NULL);
+
+  NTerm* nterm = pushdown_back(pushdown).nterm;
+
+  // check if pushdown is reduced to one nonterminal
+  if (pushdown->size == 1 && pushdown_back(pushdown).nterm != NULL) {
+    pushdown_destroy(pushdown);
+    data->type = nterm->type;
+    data->value = nterm->value;
     return true;
+  }
+
+  // syntax error occurred during parsing
+  pushdown_destroy(pushdown);
+  return false;
+}
+
+ComprarisonResult getPrecedence(PrecedenceCat pushdownItem, PrecedenceCat inputToken) {
+  return PRECEDENCE_TABLE[pushdownItem][inputToken];
+}
+
+void print_pushdown(Pushdown* pushdown) {
+  printf("St: '");
+  for (size_t i = 0; i < pushdown->size; i++) {
+    printf("%c", pushdown->data[i].name);
+  }
+  printf("'");
+}
+
+PrecedenceCat getTokenPrecedenceCategory(Token token, Token* prev_token) {
+  switch (token.type) {
+    case Token_Operator:
+      switch (token.attribute.op) {
+        case Operator_Plus:
+          return PrecendeceCat_PlusMinus;
+        case Operator_Minus:
+          if (prev_token == NULL)
+            // unary
+            return PrecendeceCat_Pre;
+
+          switch (prev_token->type) {
+            // unary
+            case Token_ParenLeft:
+            case Token_DoubleColon:
+            case Token_Comma:
+              return PrecendeceCat_Pre;
+
+            // binary
+            default:
+              return PrecendeceCat_PlusMinus;
+          }
+
+        case Operator_Multiply:
+        case Operator_Divide:
+          return PrecendeceCat_MultiDiv;
+
+        case Operator_Negation:
+          switch (prev_token->type) {
+            // postfix exclamation mark
+            case Token_Identifier:
+            case Token_Data:
+            case Token_ParenRight:
+              return PrecendeceCat_Post;
+
+            default:
+              return PrecendeceCat_Pre;
+          }
+        default:
+          return PrecendeceCat_Logic;
+      }
+
+    case Token_ParenLeft:
+      return PrecendeceCat_LeftPar;
+
+    case Token_ParenRight:
+      return PrecendeceCat_RightPar;
+
+    case Token_Identifier:
+    case Token_Data:
+      return PrecendeceCat_Id;
+
+    case Token_Comma:
+      return PrecendeceCat_Comma;
+
+    case Token_DoubleColon:
+      return PrecendeceCat_Colon;
+
+    default:
+      return PrecendeceCat_Expr_End;
+  }
+  return 0;
+}
+
+char token_to_char(PrecedenceCat cat) {
+  switch (cat) {
+    case PrecendeceCat_PlusMinus:
+      return '+';
+    case PrecendeceCat_MultiDiv:
+      return '*';
+    case PrecendeceCat_Logic:
+      return '>';
+    case PrecendeceCat_Pre:
+      return '-';
+    case PrecendeceCat_Post:
+      return '!';
+    case PrecendeceCat_LeftPar:
+      return '(';
+    case PrecendeceCat_RightPar:
+      return ')';
+    case PrecendeceCat_Id:
+      return 'i';
+    case PrecendeceCat_Comma:
+      return ',';
+    case PrecendeceCat_Colon:
+      return ':';
+    case PrecendeceCat_Expr_End:
+      return '$';
+  }
+  return '$';
+}
+
+PrecedenceCat char_to_precedence(char ch) {
+  switch (ch) {
+    case '+':
+      return PrecendeceCat_PlusMinus;
+    case '*':
+      return PrecendeceCat_MultiDiv;
+    case '>':
+      return PrecendeceCat_Logic;
+    case '-':
+      return PrecendeceCat_Pre;
+    case '!':
+      return PrecendeceCat_Post;
+    case '(':
+      return PrecendeceCat_LeftPar;
+    case ')':
+      return PrecendeceCat_RightPar;
+    case 'i':
+      return PrecendeceCat_Id;
+    case ',':
+      return PrecendeceCat_Comma;
+    case ':':
+      return PrecendeceCat_Colon;
+    default:
+      return PrecendeceCat_Expr_End;
+  }
+}
+
+void parse(Pushdown* pushdown, Token token, Token* prev_token) {
+  //   print_pushdown(pushdown);
+  int top_term_id = get_topmost_terminal_id(pushdown);
+  PrecedenceCat token_prec = getTokenPrecedenceCategory(token, prev_token);
+
+  // if there is no terminal in pushdown consider that at position -1 is '$' (PrecendeceCat_Expr_End)
+  ComprarisonResult compRes = getPrecedence(
+      top_term_id == -1 ? PrecendeceCat_Expr_End : char_to_precedence(pushdown_at(pushdown, top_term_id).name),
+      token_prec);
+
+  switch (compRes) {
+    case Left:
+      if (!reduce(pushdown))
+        break;
+      parse(pushdown, token, prev_token);
+      break;
+
+    case Right:
+      //   printf("\t >> %c\n", token_to_char(token_prec));
+      pushdown_insert(pushdown, top_term_id + 1, create_pushdown_item(NULL, NULL));  // Rule end marker
+      pushdown_push_back(pushdown, set_name(create_pushdown_item(&token, NULL), token_to_char(token_prec)));
+      parse(pushdown, scanner_advance_non_whitespace(), &token);
+      break;
+
+    case Equal:
+      //   puts("");
+
+      pushdown_push_back(pushdown, set_name(create_pushdown_item(&token, NULL), token_to_char(token_prec)));
+      parse(pushdown, scanner_advance_non_whitespace(), &token);
+      break;
+
+    case Err:
+      return;
+  }
+}
+
+bool reduce(Pushdown* pushdown) {
+  PushdownItem rule_operands[4];
+  int idx = pushdown->size;
+
+  // set idx to place where is topmost rule end
+  while (pushdown_at(pushdown, --idx).name != '|')
+    ;
+
+  int operandCount = pushdown->size - idx - 1;
+
+  char* rule = malloc((operandCount + 1) * sizeof(char));
+
+  // get rule name and operands from top of the pushdown
+  for (int i = 0; i < operandCount; i++) {
+    PushdownItem item = pushdown_at(pushdown, idx + 1 + i);
+    rule_operands[i] = item;
+    rule[i] = item.name;
+  }
+  rule[operandCount] = '\0';
+
+  // remove the rule from pushdown
+  pushdown_reduce_size(pushdown, idx);
+
+  Rule ruleName = get_rule(rule);
+  NTerm* nterm = apply_rule(ruleName, rule_operands);
+
+  // check if rule was applyed
+  if (nterm == NULL) {
+    free(rule);
+    return false;
+  }
+
+  // push new non-terminal to pushdown
+  pushdown_push_back(pushdown, set_name(create_pushdown_item(NULL, nterm), nterm->name));
+
+  free(rule);
+  return true;
+}
+
+Rule get_rule(char* rule) {
+  //   printf("\t R: %s", rule);
+  for (size_t i = 0; i < RULE_COUNT; i++) {
+    if (strcmp(rule, RULES[i]) == 0) {
+      //   printf(" -> ID: %lu \n", i);
+      return RULE_NAMES[i];
+    }
+  }
+  //   printf("NO RULE\n");
+  return NoRule;
+}
+
+NTerm* apply_rule(Rule rule, PushdownItem* operands) {
+  NTerm* nterm = malloc(sizeof(NTerm));
+  (void)operands;
+
+  switch (rule) {
+    case Rule_Identif:
+      break;
+    case Rule_Paren:
+      break;
+    case Rule_Prefix:
+      break;
+    case Rule_Postfix:
+      break;
+    case Rule_SumSub:
+      break;
+    case Rule_MulDiv:
+      break;
+    case Rule_Logic:
+      break;
+    case Rule_ArgsEE:
+      nterm->name = 'L';
+      return nterm;
+    case Rule_ArgsLE:
+      nterm->name = 'L';
+      return nterm;
+    case Rule_FnArgsProcessed:
+      break;
+    case Rule_FnArgs:
+      break;
+    case Rule_FnEmpty:
+      break;
+    case Rule_NamedArg:
+      break;
+    case NoRule:
+      return NULL;
+  }
+
+  nterm->name = 'E';
+
+  return nterm;
+}
+
+int get_topmost_terminal_id(Pushdown* pushdown) {
+  for (int i = pushdown->size - 1; i >= 0; i--)
+    if (pushdown_at(pushdown, i).terminal)
+      return i;
+  return -1;
 }
--- src/expr_parser.h
+++ src/expr_parser.h
@@ -2,20 +2,149 @@
  * @brief Declarations for precedence analysis
  * @file prec_praser.h
  * @author Jakub Kloub, xkloub03, VUT FIT
+ * @author Matúš Moravčík, xmorav48, FIT VUT
  * @date 23/11/2023
  */
 #ifndef _PREC_PARSER_H_
 #define _PREC_PARSER_H_
 
 #include <stdbool.h>
+#include "pushdown.h"
 #include "scanner.h"
 
-/**
- * @brief Begin the precedence analysis
- *
- * @param[in] first_token First token to be processed by the precedence parser
- * @return `True` if the precedence analysis succeded, `false` otherwise.
- */
-bool expr_parser_begin(Token first_token);
+#define RULE_COUNT 13
 
-#endif // _PREC_PARSER_H_
+/**
+ * @enum ComprarisonResult
+ * @brief Represents the result of the precedence comparison of the pushdown terminal and the input token.
+ */
+typedef enum {
+  Left,  /**< Higher precedence on Pushdown. */
+  Right, /**< Current token has higher precedence. */
+  Equal, /**< Equal precedence. */
+  Err,   /**< No relation between tokens => error state */
+} ComprarisonResult;
+
+/**
+ * @enum PrecedenceCat
+ * @brief Represents the result of comparison of Pushdown terminal and input token.
+ */
+typedef enum {
+  PrecendeceCat_PlusMinus, /**< + - */
+  PrecendeceCat_MultiDiv,  /**< * \/*/
+  PrecendeceCat_Logic,     /**< == != <= >= < > */
+  PrecendeceCat_Pre,       /**< - ! */
+  PrecendeceCat_Post,      /**< ! */
+  PrecendeceCat_LeftPar,   /**< ( */
+  PrecendeceCat_RightPar,  /**< ) */
+  PrecendeceCat_Id,        /**< id const */
+  PrecendeceCat_Comma,     /**< , */
+  PrecendeceCat_Colon,     /**< : */
+  PrecendeceCat_Expr_End,  /**< $ */
+} PrecedenceCat;
+
+/**
+ * @enum Rule
+ * @brief Rules for expression parsing
+ */
+typedef enum {
+  Rule_Identif,         /**< E -> i */
+  Rule_Paren,           /**< E -> (E) */
+  Rule_Prefix,          /**< E -> -E | !E */
+  Rule_Postfix,         /**< E -> id! */
+  Rule_SumSub,          /**< E -> E + E | E - E */
+  Rule_MulDiv,          /**< E -> E * E | E / E */
+  Rule_Logic,           /**< E -> E == E | E < E E && E ... */
+  Rule_ArgsEE,          /**< L -> E, E */
+  Rule_ArgsLE,          /**< L -> L, E */
+  Rule_FnArgsProcessed, /**< E -> i(L) */
+  Rule_FnArgs,          /**< E -> i(E) */
+  Rule_FnEmpty,         /**< E -> i() */
+  Rule_NamedArg,        /**< E -> i:E */
+  NoRule,
+} Rule;
+
+/**
+ * @struct NTerm
+ * @brief Rules for expression parsing
+ */
+typedef struct NTerm {
+  DataValue value; /**< resulted value after applying a reduction rule */
+  DataType type;   /**< resulted type after applying a reduction rule */
+  char name;       /**< E or L */
+} NTerm;
+
+// Forward declaration
+struct Pushdown;
+struct PushdownItem;
+
+/**
+ * @brief Starts bottom up parsing for expressions
+ * @param[in] token Current input token.
+ * @param[out] data Data of the resulting reduced nonterminal after applying operator precedence rules.
+ * @return `true` if expression was successfully parsed (reduced to just one nonterminal), otherwise `false`.
+ */
+bool expr_parser_begin(Token token, Data* data);
+
+/**
+ * @brief Classify `token` to precedence category. ambiguous token as - or ! are decided be `prev_token`
+ * @param[in] token Current input token.
+ * @param[in] prev_token Token from the last step.
+ * @return `token` precedence category.
+ */
+PrecedenceCat getTokenPrecedenceCategory(Token token, Token* prev_token);
+
+/**
+ * @brief Get precendence from the precedence table
+ * @param[in] pushdownItem Pushdown precedence index.
+ * @param[in] inputToken Input token precedence index.
+ * @return `ComparisonResult`.
+ */
+ComprarisonResult getPrecedence(PrecedenceCat pushdownItem, PrecedenceCat inputToken);
+
+/**
+ * @brief Convert precendence category into its character representation.
+ * @param[in] cat Precendence category.
+ * @return Character representation of a given precedence category.
+ */
+char token_to_char(PrecedenceCat cat);
+
+/**
+ * @brief Recursively parse expression until there is `Err` between topmost pushdown terminal and input terminal.
+ * @param[in, out] pushdown Initialized pushdown.
+ * @param[in] token Token to be processed.
+ * @param[in] prev_token Previous token for deciding ambiguous tokens precedence category.
+ */
+void parse(struct Pushdown* pushdown, Token token, Token* prev_token);
+
+/**
+ * @brief Decide which rule should be used for a given string
+ * @param[in] rule String representation of right side of rule.
+ * @return Corresponding `Rule` or NoRule if no match was found.
+ */
+Rule get_rule(char* rule);
+
+/**
+ * @brief Create non terminal by applying rule to `operands`.
+ * @param[in] rule Rule that is used to reduce operand to a new non terminal.
+ * @param[in] operands operands needed for reduction.
+ * @return Non terminal that holds its type and value infered from `operands` by applying `rule`.
+ */
+NTerm* apply_rule(Rule rule, struct PushdownItem* operands);
+
+/**
+ * @brief Remove all terminals and nonterminals from `pushdown` and replace them by non terminal created by applying any
+ * rule.
+ * @param[in, out] pushdown Initialized pushdown.
+ * @return `true` if reduction was performed. otherwise `false`.
+ */
+bool reduce(struct Pushdown* pushdown);
+
+/**
+ * @brief Retrieves the index of the topmost terminal in the Pushdown structure.
+ * @param[in] pushdown Pointer to the Pushdown structure.
+ * @return The index of the topmost terminal, or -1 if no terminal is found.
+ */
+int get_topmost_terminal_id(struct Pushdown* pushdown);
+
+#endif  // _PREC_PARSER_H_
