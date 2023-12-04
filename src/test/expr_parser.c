40d3a10bb6fb7498c4539e9eb3eacbbe9bf9ce25
Blazeo7 <moravcikamtus343@gmail.com>
2023-12-04 22:43:06 +0100

    Update tests

--- src/test/expr_parser.c
+++ src/test/expr_parser.c
@@ -7,6 +7,7 @@
 
 #include "../expr_parser.h"
 #include <string.h>
+#include "../codegen.h"
 #include "../parser.h"
 #include "../scanner.h"
 #include "test.h"
@@ -34,50 +35,58 @@
     do {                                                       \
         VariableSymbol name;                                   \
         variable_symbol_init(&name);                           \
+        name.code_frame = Frame_Local;                         \
+        string_concat_c_str(&name.code_name, #name);           \
         name.is_initialized = true;                            \
         name.type = dt;                                        \
         symtable_insert_variable(symstack_top(), #name, name); \
     } while (0)
 
-void insert_functions() {
-    FunctionSymbol fn;
-    FunctionSymbol many;
-    FunctionSymbol x;
-    FunctionSymbol str;
+#define INSERT_FUNCTION_WITH_ARGS(name, dt, ...)                                                       \
+    do {                                                                                               \
+        FunctionSymbol name;                                                                           \
+        function_symbol_init(&name);                                                                   \
+        name.return_value_type = dt;                                                                   \
+        string_concat_c_str(&name.code_name, #name);                                                   \
+        Param params[] = {__VA_ARGS__};                                                                \
+        for (size_t i = 0; i < sizeof(params) / sizeof(params[0]); i++) {                              \
+            function_symbol_emplace_param(&name, (params[i]).param_dt, (params[i]).param_name, #name); \
+        }                                                                                              \
+        symtable_insert_function(symstack_top(), #name, name);                                         \
+    } while (0)
 
-    function_symbol_init(&fn);
-    function_symbol_init(&many);
-    function_symbol_init(&x);
-    function_symbol_init(&str);
+#define INSERT_FUNCTION_WITHOUT_ARGS(name, dt)                 \
+    do {                                                       \
+        FunctionSymbol name;                                   \
+        function_symbol_init(&name);                           \
+        name.return_value_type = dt;                           \
+        symtable_insert_function(symstack_top(), #name, name); \
+    } while (0)
 
-    fn.return_value_type = DataType_MaybeDouble;
-    many.return_value_type = DataType_Double;
-    x.return_value_type = DataType_Int;
-    str.return_value_type = DataType_String;
-
-    function_symbol_emplace_param(&fn, DataType_Int, "x", "x");
-    function_symbol_emplace_param(&fn, DataType_Double, "y", "y");
-
-    function_symbol_emplace_param(&many, DataType_Int, "int", "x");
-    function_symbol_emplace_param(&many, DataType_Double, NULL, "d");
-    function_symbol_emplace_param(&many, DataType_MaybeInt, NULL, "mint");
-    function_symbol_emplace_param(&many, DataType_MaybeDouble, NULL, "md");
-
-    function_symbol_emplace_param(&str, DataType_String, NULL, "a");
-    function_symbol_emplace_param(&str, DataType_String, NULL, "b");
-
-    symtable_insert_function(symstack_top(), "fn", fn);
-    symtable_insert_function(symstack_top(), "many", many);
-    symtable_insert_function(symstack_top(), "x", x);
-    symtable_insert_function(symstack_top(), "str", str);
-}
+typedef struct {
+    char* param_name;
+    DataType param_dt;
+} Param;
 
 int main() {
     atexit(summary);
+    Data expr_data;
+    CodeBuf buf;
 
     parser_init();
+    code_buf_init(&buf);
+    code_buf_set(&buf);
 
-    insert_functions();
+    INSERT_FUNCTION_WITHOUT_ARGS(x, DataType_Int);
+    INSERT_FUNCTION_WITHOUT_ARGS(n, DataType_Undefined);
+    INSERT_FUNCTION_WITH_ARGS(fn, DataType_MaybeDouble, {.param_name = "x", .param_dt = DataType_Int},
+                              {.param_name = "y", .param_dt = DataType_Double});
+    INSERT_FUNCTION_WITH_ARGS(many, DataType_Double, {.param_name = "int", .param_dt = DataType_Int},
+                              {.param_name = NULL, .param_dt = DataType_Double},
+                              {.param_name = NULL, .param_dt = DataType_MaybeInt},
+                              {.param_name = NULL, .param_dt = DataType_MaybeDouble});
+    INSERT_FUNCTION_WITH_ARGS(str, DataType_String, {.param_name = NULL, .param_dt = DataType_String},
+                              {.param_name = NULL, .param_dt = DataType_String});
 
     INSERT_VARIABLE(a, DataType_Int);
     INSERT_VARIABLE(b, DataType_Double);
@@ -86,14 +95,14 @@ int main() {
     INSERT_VARIABLE(bl, DataType_MaybeBool);
     INSERT_VARIABLE(s, DataType_MaybeString);
 
-    Data expr_data;
-
     suite("Test valid arithmetic expressions") {
         TEST_VALID_EXPRESSION("nil", DataType_Undefined);
-        TEST_VALID_EXPRESSION("1", DataType_Int);
+        TEST_VALID_EXPRESSION("(a)", DataType_Int);
+        TEST_VALID_EXPRESSION("(1)", DataType_Int);
         TEST_VALID_EXPRESSION("\"adsf\"", DataType_String);
         TEST_VALID_EXPRESSION("1 + 2 * 3", DataType_Int);
         TEST_VALID_EXPRESSION("1.2 + 2", DataType_Double);
+        TEST_VALID_EXPRESSION("1 - 1.4", DataType_Double);
         TEST_VALID_EXPRESSION("\"fsd\" + \"45\"", DataType_String);
         TEST_VALID_EXPRESSION("a * 1.4", DataType_Int);
         TEST_VALID_EXPRESSION("b / 1.4", DataType_Double);
@@ -103,7 +112,9 @@ int main() {
     }
 
     suite("Test valid logic expressions") {
-        TEST_VALID_EXPRESSION("1 < 0", DataType_Bool);
+        TEST_VALID_EXPRESSION("!true", DataType_Bool);
+        TEST_VALID_EXPRESSION("bl!", DataType_Bool);
+        TEST_VALID_EXPRESSION("1 <= 0.0", DataType_Bool);
         TEST_VALID_EXPRESSION("\"adsf\" == \"fdsaf\"", DataType_Bool);
         TEST_VALID_EXPRESSION("!true", DataType_Bool);
         TEST_VALID_EXPRESSION("true == true", DataType_Bool);
@@ -120,6 +131,8 @@ int main() {
         TEST_VALID_EXPRESSION("1 + many(int: a, b, y, 5.5) * 2", DataType_Double);
         TEST_VALID_EXPRESSION("x() != x() * 2", DataType_Bool);
         TEST_VALID_EXPRESSION("str(name, name) + \"String\"", DataType_String);
+        TEST_VALID_EXPRESSION("x()", DataType_Int);
+        TEST_VALID_EXPRESSION("n()", DataType_Undefined);
     }
 
     suite("Test valid nil coalescing") {
@@ -149,6 +162,7 @@ int main() {
         TEST_INVALID_EXPRESSION(" 1!", Error_Operation);
         TEST_INVALID_EXPRESSION("a + b", Error_Operation);
         TEST_INVALID_EXPRESSION("1 + nil", Error_UnknownType);
+        TEST_INVALID_EXPRESSION("true + true", Error_Operation);
         TEST_INVALID_EXPRESSION("nil ?? 45", Error_UnknownType);
     }
 
@@ -161,6 +175,9 @@ int main() {
         TEST_INVALID_EXPRESSION("!nil", Error_UnknownType);
         TEST_INVALID_EXPRESSION("!nil!", Error_UnknownType);
         TEST_INVALID_EXPRESSION("y ?? !!false", Error_Syntax);
+        TEST_INVALID_EXPRESSION("true < false", Error_Operation);
+        TEST_INVALID_EXPRESSION("n() == nil", Error_UnknownType);
+        TEST_INVALID_EXPRESSION("nil == nil", Error_UnknownType);
     }
 
     suite("Test invalid nil coaliscing expressions") {
@@ -185,10 +202,13 @@ int main() {
         TEST_INVALID_EXPRESSION("u", Error_UndefinedVariable);
         TEST_INVALID_EXPRESSION("a - if", Error_Syntax);
         TEST_INVALID_EXPRESSION("a()", Error_UndefinedFunction);
-        TEST_INVALID_EXPRESSION("45()", Error_UndefinedFunction);
+        TEST_INVALID_EXPRESSION("45()", Error_Syntax);
         TEST_INVALID_EXPRESSION("strv()", Error_UndefinedFunction);
     }
 
+    puts("");
+    // code_buf_print(&buf);
+    code_buf_free(&buf);
     parser_free();
     return 0;
 }
